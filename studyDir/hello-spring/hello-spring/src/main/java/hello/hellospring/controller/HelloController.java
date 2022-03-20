dc08615022fa33ac29a5468d2003b96f4a6820b1
jstop01 <jstop01@naver.com>
2022-03-21 08:29:51 +0900

    깃 오류 수정

--- studyDir/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ studyDir/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -35,6 +35,7 @@ public class HelloController {
     @GetMapping("hello-api")
     @ResponseBody
     public Hello helloApi(@RequestParam("name") String name) {
+
         Hello hello = new Hello();
         hello.setName(name);
         return hello;
